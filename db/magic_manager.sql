8e4766f7c3e41adea6262691209f72ebbf1f6883
Robyn Tyler <robynstyler@outlook.com>
2020-11-29 15:33:56 +0000

    Created tables wizards and locations, created dabatase.

--- db/magic_manager.sql
+++ db/magic_manager.sql
@@ -1,6 +1,8 @@
 -- Database table info goes here
 
 DROP TABLE IF EXISTS wizards;
+DROP TABLE IF EXISTS locations;
+DROP TABLE IF EXISTS spells;
 
 CREATE TABLE wizards (
     id SERIAL PRIMARY KEY,
@@ -8,3 +10,11 @@ CREATE TABLE wizards (
     last_name VARCHAR(255),
     age INT
 );
+
+CREATE TABLE locations (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    type VARCHAR(255),
+    realm VARCHAR(255)
+);
+
