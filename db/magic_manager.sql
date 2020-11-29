5c758e8deebde7d992f0dcb6f709ee1e4bf55728
Robyn Tyler <robynstyler@outlook.com>
2020-11-29 15:24:32 +0000

    Changed castings to casts

--- db/magic_manager.sql
+++ db/magic_manager.sql
@@ -1,1 +1,10 @@
 -- Database table info goes here
+
+DROP TABLE IF EXISTS wizards;
+
+CREATE TABLE wizards (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    age INT
+);
